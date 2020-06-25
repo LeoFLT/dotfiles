2d43fff02f7fa7b7c16472ebba8ac39fb503a22b
LeoFLT <leo43230@gmail.com>
2020-06-25 00:54:09 -0300

    edits

--- .config/polybar/spotify.sh
+++ .config/polybar/spotify.sh
@@ -1,7 +1,7 @@
 if [[ -n `pgrep spotify` ]]; then
     if [[ `playerctl status -p spotify` == "Playing" ]]; then
-        echo " `playerctl -p spotify metadata -f '{{artist}} - {{title}}'`"
+        echo "  `playerctl -p spotify metadata -f '{{artist}} - {{title}}'`"
     elif [[ `playerctl status -p spotify` == "Paused" ]]; then
-        echo " `playerctl -p spotify metadata -f '{{artist}} - {{title}}'`"
+        echo "  `playerctl -p spotify metadata -f '{{artist}} - {{title}}'`"
     fi
 fi
