26da05f0f239c4d062dce152b2dc502f75065508
LeoFLT <leo43230@gmail.com>
2020-06-26 01:16:55 -0300

    improved spotify.sh script

--- .config/polybar/spotify.sh
+++ .config/polybar/spotify.sh
@@ -1,7 +1,7 @@
-if [[ -n `pgrep spotify` ]]; then
-    if [[ `playerctl status -p spotify` == "Playing" ]]; then
-        echo "  `playerctl -p spotify metadata -f '{{artist}} - {{title}}'`"
-    elif [[ `playerctl status -p spotify` == "Paused" ]]; then
-        echo "  `playerctl -p spotify metadata -f '{{artist}} - {{title}}'`"
-    fi
+if [[ -z `pgrep spotify` ]]; then
+	echo "";
+elif [[ `playerctl status -p spotify` == "Playing" ]]; then
+      	echo "  `playerctl -p spotify metadata -f '{{artist}} - {{title}}'`";
+elif [[ `playerctl status -p spotify` == "Paused" ]]; then
+      	echo "  `playerctl -p spotify metadata -f '{{artist}} - {{title}}'`";
 fi
